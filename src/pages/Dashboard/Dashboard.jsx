d538668a0a68a08acf9ad4cd7802b7a5f8d9cf33
robbie-carragher <orcafinweb@gmail.com>
2024-01-04 21:34:35 -0800

    nowPlaying complete

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,202 +1,4 @@
 
-// import { useState, useEffect } from "react";
-// import useAuth from "../../components/useAuth";
-// import Player from "../../components/Player";
-// import TrackSearchResult from "../../components/TrackSearchResult/TrackSearchResult";
-// import { Form } from "react-bootstrap";
-// import SpotifyWebApi from "spotify-web-api-node";
-// import axios from "axios";
-// import "./Dashboard.scss";
-// import backgroundVideo from "../../../src/assets/images/backVideo.mp4";
-
-// const spotifyApi = new SpotifyWebApi({
-//   clientId: "7fca14558bdf4a21a907c174dcf86239",
-// });
-
-// export default function Dashboard({ code }) {
-//   const accessToken = useAuth(code);
-//   const [search, setSearch] = useState("");
-//   const [searchResults, setSearchResults] = useState([]);
-//   const [playingTrack, setPlayingTrack] = useState();
-//   const [lyrics, setLyrics] = useState("");
-//   const [playlist, setPlaylist] = useState(null);
-//   const [userProfile, setUserProfile] = useState(null);
-
-//   useEffect(() => {
-//     if (!accessToken) return;
-//     spotifyApi.setAccessToken(accessToken);
-  
-//     // Fetch user's profile
-//     spotifyApi.getMe().then((data) => {
-//       setUserProfile(data.body);
-//     }).catch(err => console.error(err));
-//   }, [accessToken]);
-
-
-
-
-//   useEffect(() => {
-//     if (!accessToken) return;
-//     spotifyApi.setAccessToken(accessToken);
-//     spotifyApi.getPlaylist('3HAVA7fMXxAIObFBlXc9B7') 
-//       .then(data => {
-//         setPlaylist(data.body);
-//       })
-//       .catch(err => console.error(err));
-//   }, [accessToken]);
-
-//   useEffect(() => {
-//     if (!accessToken) return;
-//     spotifyApi.setAccessToken(accessToken);
-//     if (search === "") {
-//       spotifyApi.searchTracks("a").then((res) => {
-//         setSearchResults(
-//           res.body.tracks.items.map((track) => {
-//             const smallestAlbumImage = track.album.images.reduce(
-//               (smallest, image) => {
-//                 if (image.height < smallest.height) return image;
-//                 return smallest;
-//               },
-//               track.album.images[0]
-//             );
-//             return {
-//               artist: track.artists[0].name,
-//               title: track.name,
-//               uri: track.uri,
-//               albumUrl: smallestAlbumImage.url,
-//             };
-//           })
-//         );
-//       });
-//     }
-//   }, [accessToken, search]);
-
-//   useEffect(() => {
-//     if (!search) return;
-//     if (!accessToken) return;
-//     let cancel = false;
-//     spotifyApi.searchTracks(search).then((res) => {
-//       if (cancel) return;
-//       setSearchResults(
-//         res.body.tracks.items.map((track) => {
-//           const smallestAlbumImage = track.album.images.reduce(
-//             (smallest, image) => {
-//               if (image.height < smallest.height) return image;
-//               return smallest;
-//             },
-//             track.album.images[0]
-//           );
-//           return {
-//             artist: track.artists[0].name,
-//             title: track.name,
-//             uri: track.uri,
-//             albumUrl: smallestAlbumImage.url,
-//           };
-//         })
-//       );
-//     });
-//     return () => {
-//       cancel = true;
-//     };
-//   }, [search, accessToken]);
-
-//   function chooseTrack(track) {
-//     setPlayingTrack(track);
-//     setSearch("");
-//     setLyrics("");
-//     axios
-//       .get("http://localhost:3001/lyrics", {
-//         params: {
-//           track: track.title,
-//           artist: track.artist,
-//         },
-//       })
-//       .then((res) => {
-//         setLyrics(res.data.lyrics);
-//       });
-//   }
-
-//   const renderPlaylistTracks = (playlist) => {
-//     return playlist.tracks.items.slice(0,6).map((item, index) => {
-//       const track = item.track;
-//       return (
-//         <div key={index} className="playlist-track" onClick={() => chooseTrack(track)}>
-//           <img 
-//             src={track.album.images[0].url} 
-//             alt={track.album.name} 
-//             className="playlist-track__album-cover"
-//           />
-//           <div className="playlist-track__track-details">
-//             <div className="playlist-track__track-title">{track.name}</div>
-//             <div className="playlist-track__track-artist">{track.artists.map(artist => artist.name).join(", ")}</div>
-//           </div>
-//         </div>
-//       );
-//     });
-//   };
-
-//   return (
-//     <div className="dashStyle">
-//       <div className="dashStyle__outer-wrap">
-//         <div className="dashStyle__inner-wrap">
-
-//           <div className="dashStyle__search">
-// <div className="dashStyle__search-flex">
-// <div className="dashStyle__btn">
-//     <a href="/">LOGOUT</a>
-//   </div>
-//   <div className="dashStyle__search-input">
-//     <Form.Control
-//       type="search"
-//       placeholder="Search Songs/Artists"
-//       value={search}
-//       onChange={(e) => setSearch(e.target.value)}
-//     />
-//   </div>
-// </div>
-
-//   <div className="dashStyle__playlist">
-//     {playlist && (
-//       <div className="dashStyle__playlist-wrap">
-//         <h2 className="dashStyle__playlist-name">{playlist.name}</h2>
-//         <div className="dashStyle__playlist-playlist">{renderPlaylistTracks(playlist)}</div>
-//       </div>
-//     )}
-//   </div>
-// </div>
-
-//           <div className="dashStyle__lyric">
-//             <video autoPlay muted loop className="dashStyle__video">
-//               <source src={backgroundVideo} type="video/mp4" />
-//             </video>
-//             <h1 className="dashStyle__title">Welcome To Lyric Spot</h1>
-//             <div className="dashStyle__lyric-result">
-//               {lyrics !== "" ? lyrics : "Search for a song to see lyrics here."}
-//             </div>
-//           </div>
-//         </div>
-//         <div className="dashStyle__map">
-//           {searchResults.slice(0, 8).map((track) => (
-//             <TrackSearchResult
-//               track={track}
-//               key={track.uri}
-//               chooseTrack={chooseTrack}
-//             />
-//           ))}
-//         </div>
-
-//       </div>
-//       <div className="player">
-//         <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
-//       </div>
-//     </div>
-
-
-
-
-    
-//   );
-// }
 import { useState, useEffect } from "react";
 import useAuth from "../../components/useAuth";
 import Player from "../../components/Player";
@@ -226,6 +28,18 @@ export default function Dashboard({ code }) {
     spotifyApi.getMe().then((data) => {
       setUserProfile(data.body);
     }).catch(err => console.error(err));
+
+    const defaultTrackId = "1TKBTEgnXj3ob26HwJDK7P"; // Replace with a valid Spotify track ID
+    spotifyApi.getTrack(defaultTrackId).then(res => {
+      const trackData = res.body;
+      setPlayingTrack({
+        artist: trackData.artists[0].name,
+        title: trackData.name,
+        uri: trackData.uri,
+        albumUrl: trackData.album.images[0].url,
+      });
+    }).catch(err => console.error(err));
+    
   }, [accessToken]);
 
   useEffect(() => {
@@ -389,6 +203,7 @@ export default function Dashboard({ code }) {
 
         </div>
         <div className="dashStyle__map">
+        <defaultTrackId />
         <CurrentlyPlaying track={playingTrack} />
           {searchResults.slice(0, 8).map((track) => (
             <TrackSearchResult track={track} key={track.uri} chooseTrack={chooseTrack} />
