e12db3f4877c9ebca83d279571a43c0d4585a1ed
robbie-carragher <orcafinweb@gmail.com>
2024-01-07 02:44:53 -0800

    Card 80% complete for render

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,6 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <App />
-  // </React.StrictMode>,
+//  </React.StrictMode>,
 )
 
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import { Provider } from 'react-redux'
-// import App from './App.jsx'
-// import store from './store' // Import your Redux store
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//  </React.StrictMode>,
-// )
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import "./Login.scss"
 import backgroundVideo from "../../../src/assets/images/backVideo.mp4";
-  
+
   import "./Login.scss";
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=7fca14558bdf4a21a907c174dcf86239&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private%20playlist-read-collaborative%20user-top-read%20user-read-currently-playing%20user-read-recently-played%20user-follow-read";
